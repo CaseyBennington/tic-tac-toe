abcb2079a18d4b05bc52f05b8b3a57fb42e79492
Jami Gibbs <jami0821@gmail.com>
2016-04-14 11:17:01 -0500

    Adding functionality

--- README.md
+++ README.md
@@ -1,8 +1,65 @@
 ### Tic Tac Toe
 
-A single player version of the classic [Tic Tac Toe](https://en.wikipedia.org/wiki/Tic-tac-toe) game.
+The classic game of [Tic Tac Toe](https://en.wikipedia.org/wiki/Tic-tac-toe) game. Human vs. Computer.
 
 <!-- ![preview](preview.jpg) -->
+
+### The Logic
+
+Each square is assigned a bit value with successive powers of 2:
+
+```
+
+  273        84
+  |           |
+
+  1  |  2  |  4   - 7
+  --- ----- ----
+  8  |  16 |  32  - 56
+  --- ----- ----
+  64 | 128 | 256  - 448
+
+  |     |     |
+  73   146   292
+
+```
+
+The cells each represent an individual bit in a 9-bit string:
+
+```
+
+  1   = 000000001
+  2   = 000000010
+  4   = 000000100
+  ...
+  256 = 100000000
+
+```
+
+Winning combinations will include the 3 winning bits:
+
+```
+
+  7   = 000000111
+  56  = 000111000
+  73  = 001001001
+  ...
+  448 = 111000000
+
+```
+
+Using the bitwise operation, we compare the user's total score with the winning combinations to check for 3 matching bits (ie. a winner):
+
+Example scenario:
+
+```
+
+    000010111 (23)  // The user has a mark on the 1, 16, 4, & 2 squares
+  & 000000111 (7)   // Three of the required bits match resulting in a winning row
+    ---------
+    000000111 (7)   // After evaluating the bit strings, a winning row is determined
+
+```
 
 ### Local Setup
 
--- js/app.js
+++ js/app.js
@@ -3,7 +3,124 @@
 
   $(document).ready(function(){
 
+    // Set our variables
+    var boardCells = document.getElementsByClassName('cell'),
+        wins = [7, 56, 448, 73, 146, 292, 273, 84],
+        turn = 'X',
+        score = {'X': 0, 'O': 0},
+        turnCount = 0,
+        moves = 0,
+        finished = false;
 
+    function winner(score) {
+      // Loop through the possible winning scores
+      for (var i = 0; i < wins.length; i += 1) {
+        // Our bitwise comparison
+        if ((wins[i] & score) === wins[i]) {
+          return true;
+        }
+      }
+
+      // If the move played isn't a winning play
+      return false;
+    }
+
+    function computerMove(){
+      // Switch to computer's mark
+      turn = 'O';
+      moves += 1;
+      var emptyCells = [];
+
+      // Create an array of empty cells
+      for(var i = 0; i < boardCells.length; i++){
+        if(!boardCells[i].innerHTML){
+          emptyCells.push(i);
+        }
+      }
+
+      // Get a random empty cell number
+      var random = emptyCells[ Math.floor( Math.random() * emptyCells.length) ];
+
+      // Place the computer's mark in the empty cells
+      boardCells[random].innerHTML = turn;
+
+      // Add to the computer's score
+      score[turn] += parseInt( boardCells[random].dataset.bit );
+
+      scoreCheck();
+
+      // Switch back to user's mark
+      turn = 'X';
+    }
+
+    function humanMove(position){
+      moves += 1;
+
+      // User Move (default is 'X')
+      $(position).text(turn);
+
+      // Add to the humman's score
+      score[turn] += parseInt( $(position).data('bit') );
+
+      scoreCheck();
+    }
+
+    function scoreCheck(){
+      // Check if the current score is a winning score
+      if ( winner(score[turn]) ) {
+        finished = true;
+        $('#notice').text(turn + ' wins!');
+      } else if (moves === 9) {
+        finished  = true;
+        $('#notice').text('Cat\u2019s game!');
+      } else {
+        return;
+      }
+    }
+
+    function setMove(position){
+      // Alert user if space is already taken
+      if( $(position).text() ){
+        alert('Cell already taken!');
+        return;
+      } else {
+
+        // Human
+        humanMove(position);
+
+        // AI
+        if(!finished){
+          computerMove();
+        }
+
+      }
+    }
+
+    function resetGame() {
+      // Reset the vars
+      turn = 'X';
+      score = {'X': 0, 'O': 0};
+      moves = 0;
+
+      // Clear the board
+      for (var i = 0; i < boardCells.length; i++) {
+        boardCells[i].innerHTML = '';
+      }
+
+      // Clear the notice text
+      $('#notice').text('');
+      finished = false;
+    }
+
+    $('.cell').click(function(){
+      if(!finished){
+        setMove(this);
+      }
+    });
+
+    $('#new-game').click(function(){
+      resetGame();
+    });
 
   });
 
