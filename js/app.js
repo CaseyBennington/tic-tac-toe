03e9286715f17efa06d6bc4066d0b2e205db2058
Jami Gibbs <jami0821@gmail.com>
2016-04-26 14:24:11 -0500

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,39 +1,57 @@
 (function(){
   'use strict';
 
-  $(document).ready(function(){
+  var TicTacToe = {
 
-    // Set our variables
-    var boardCells = document.getElementsByClassName('cell'),
-        wins = [7, 56, 448, 73, 146, 292, 273, 84],
-        turn = 'X',
-        score = {'X': 0, 'O': 0},
-        turnCount = 0,
-        moves = 0,
-        finished = false;
+    boardCells: document.getElementsByClassName('cell'),
+    wins: [7, 56, 448, 73, 146, 292, 273, 84],
+    turn: 'X',
+    score: {'X': 0, 'O': 0},
+    turnCount: 0,
+    moves: 0,
+    finished: false,
 
-    function winner(score) {
+    init: function(){
+      var that = this;
+
+      $('.cell').on('click', function(){
+        if(!that.finished){
+          that.setMove(this);
+        }
+      });
+
+      $('#new-game').on('click', function(){
+        that.resetGame();
+      });
+
+    },
+
+    winner: function(score){
+      var that = this;
+
       // Loop through the possible winning scores
-      for (var i = 0; i < wins.length; i += 1) {
+      for (var i = 0; i < that.wins.length; i += 1) {
         // Our bitwise comparison
-        if ((wins[i] & score) === wins[i]) {
+        if ( (that.wins[i] & score) === that.wins[i] ) {
           return true;
         }
       }
 
       // If the move played isn't a winning play
       return false;
-    }
+    },
 
-    function computerMove(){
+    computerMove: function(){
+      var that = this;
+
       // Switch to computer's mark
-      turn = 'O';
-      moves += 1;
+      that.turn = 'O';
+      that.moves += 1;
       var emptyCells = [];
 
       // Create an array of empty cells
-      for(var i = 0; i < boardCells.length; i++){
-        if(!boardCells[i].innerHTML){
+      for(var i = 0; i < that.boardCells.length; i++){
+        if(!that.boardCells[i].innerHTML){
           emptyCells.push(i);
         }
       }
@@ -44,46 +62,52 @@
       // Place the computer's mark in the empty cells
       // Add to the computer's score
       setTimeout(function(){
-        boardCells[random].innerHTML = turn;
-        boardCells[random].className += ' alt';
+        that.boardCells[random].innerHTML = that.turn;
+        that.boardCells[random].className += ' alt';
 
-        score[turn] += parseInt( boardCells[random].dataset.bit );
+        that.score[that.turn] += parseInt( that.boardCells[random].dataset.bit );
 
-        scoreCheck();
+        that.scoreCheck();
 
         // Switch back to user's mark
-        turn = 'X';
+        that.turn = 'X';
 
       }, 500);
 
-    }
+    },
 
-    function humanMove(position){
-      moves += 1;
+    humanMove: function(position){
+      var that = this;
+
+      that.moves += 1;
 
       // User Move (default is 'X')
-      $(position).text(turn);
+      $(position).text(that.turn);
 
       // Add to the humman's score
-      score[turn] += parseInt( $(position).data('bit') );
+      that.score[that.turn] += parseInt( $(position).data('bit') );
 
-      scoreCheck();
-    }
+      that.scoreCheck();
+    },
 
-    function scoreCheck(){
+    scoreCheck: function(){
+      var that = this;
+
       // Check if the current score is a winning score
-      if ( winner(score[turn]) ) {
-        finished = true;
-        $('#notice').text(turn + ' wins!');
-      } else if (moves === 9) {
-        finished  = true;
+      if ( that.winner(that.score[that.turn]) ) {
+        that.finished = true;
+        $('#notice').text(that.turn + ' wins!');
+      } else if (that.moves === 9) {
+        that.finished  = true;
         $('#notice').text('Tie game!');
       } else {
         return;
       }
-    }
+    },
 
-    function setMove(position){
+    setMove: function(position){
+      var that = this;
+
       // Alert user if space is already taken
       if( $(position).text() ){
         alert('Cell already taken!');
@@ -91,43 +115,39 @@
       } else {
 
         // Human
-        humanMove(position);
+        that.humanMove(position);
 
         // AI
-        if(!finished){
-          computerMove();
+        if(!that.finished){
+          that.computerMove();
         }
 
       }
-    }
+    },
 
-    function resetGame() {
+    resetGame: function() {
+      var that = this;
+
       // Reset the vars
-      turn = 'X';
-      score = {'X': 0, 'O': 0};
-      moves = 0;
+      that.turn = 'X';
+      that.score = {'X': 0, 'O': 0};
+      that.moves = 0;
 
       // Clear the board
-      for (var i = 0; i < boardCells.length; i++) {
-        boardCells[i].innerHTML = '';
-        boardCells[i].className = 'cell';
+      for (var i = 0; i < that.boardCells.length; i++) {
+        that.boardCells[i].innerHTML = '';
+        that.boardCells[i].className = 'cell';
       }
 
       // Clear the notice text
       $('#notice').text('');
-      finished = false;
-    }
+      that.finished = false;
+    },
 
-    $('.cell').click(function(){
-      if(!finished){
-        setMove(this);
-      }
-    });
+  };
 
-    $('#new-game').click(function(){
-      resetGame();
-    });
-
+  $(document).ready(function(){
+    TicTacToe.init();
   });
 
 }());
